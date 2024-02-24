79d29d74f838f78743f97b8b39317b3d486b63d6
Polvere243 <danielercoli@gmail.com>
2024-02-24 12:41:18 +0100

    printed in page after fixing some things here and there

--- data/movies.php
+++ data/movies.php
@@ -1,8 +1,13 @@
 <?php
+
+require __DIR__ . '/../models/Movie.php'; 
+
 $batman_line = 'Dimmi: tu sanguini? Gronderai!';
 
-$batman_line_j = 'Non m\'importa quanti demoni in quanti inferni. Non ha mai combattuto con noi. Non con noi uniti';
+$batman_line_j = 'Non m\'importa quanti demoni in quanti inferni. Non ha mai combattuto con noi. Non con noi uniti.';
 
-$batman_v_superman = new Movies ('Batman v Superman, dawn of justice', '181 min', '874362803', $batman_line);
+$batman_v_superman = new Movies ('Batman v Superman, dawn of justice', '181 min', $batman_line, '874362803');
 
 $justice_league = new Movies ('Zack Snyder \' Justice League', '242 min', $batman_line_j);
+
+$movies = [$batman_v_superman, $justice_league];
--- index.php
+++ index.php
@@ -1,4 +1,28 @@
 <?php
 
+require __DIR__ . '/data/movies.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="author" content="Daniele Ercoli">
+    <meta name="description" content="esercizio">
+    <meta name="keyword" content="php, html">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+<body>
+    <h1>FILM</h1>
+    <?php foreach ($movies as $movie) : ?> 
+        <h2>Titolo: <?=$movie ->title?></h2>
+        <h4>Durata: <?=$movie ->duration?></h4>
+        <h4>Incasso: <?=$movie ->earning?></h4>
+        <p><strong>Battuta Epica: </strong><?=$movie ->line?></p>
+    <?php endforeach ?>
+</body>
+</html>
 
 
