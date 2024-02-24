cc208e5a6427b00a8ac673fed2d340d82a9decdc
Polvere243 <danielercoli@gmail.com>
2024-02-24 11:19:15 +0100

    defined a method, hoping it works

--- index.php
+++ index.php
@@ -6,12 +6,18 @@ class Movies {
     public $plot;
     public $duration;
     public $earning;
+    public $line;
     
     // costruttore
-    public function __construct($title, $plot, $duration, $earning) {
+    public function __construct($title, $plot, $duration, $earning, $line) {
         $this ->title = $title;
         $this ->plot = $plot;
         $this ->duration = $duration;
         $this ->earning = $earning;
+        $this ->line = $line;
+    }
+
+    public function printALine() {
+        echo $this->line;
     }
 }
