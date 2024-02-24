995af7a37bc1845cbd0e36463d468488bbd0b070
Polvere243 <danielercoli@gmail.com>
2024-02-24 12:04:32 +0100

    put the line into a variable

--- index.php
+++ index.php
@@ -22,8 +22,12 @@ class Movies
     }
 }
 
-$batman_v_superman = new Movies ('Batman v Superman, dawn of justice', '181 min', '874362803', 'Dimmi: tu sanguini? Gronderai!');
+$batman_line = 'Dimmi: tu sanguini? Gronderai!';
 
-$justice_league = new Movies ('Zack Snyder \' Justice League', '242 min', 'Non m\'importa quanti demoni in quanti inferni. Non ha mai combattuto con noi. Non con noi uniti');
+$batman_line_j = 'Non m\'importa quanti demoni in quanti inferni. Non ha mai combattuto con noi. Non con noi uniti';
+
+$batman_v_superman = new Movies ('Batman v Superman, dawn of justice', '181 min', '874362803', $batman_line);
+
+$justice_league = new Movies ('Zack Snyder \' Justice League', '242 min', $batman_line_j);
 
 var_dump($batman_v_superman, $justice_league);
