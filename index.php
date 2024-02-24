0bca4b2251fdd303ff73011afd34abf1202da9c6
Polvere243 <danielercoli@gmail.com>
2024-02-24 11:43:26 +0100

    hoped that I printed properly

--- index.php
+++ index.php
@@ -9,11 +9,11 @@ class Movies
     public $line;
     
     // costruttore
-    public function __construct($title, $duration, $earning = null, $line) {
+    public function __construct($title, $duration, $line, $earning = null) {
         $this ->title = $title;
         $this ->duration = $duration;
-        $this ->earning = $earning;
         $this ->line = $line;
+        $this ->earning = $earning;
     }
 
     public function printALine() {
@@ -25,3 +25,4 @@ $batman_v_superman = new Movies ('Batman v Superman, dawn of justice', '181 min'
 
 $justice_league = new Movies ('Zack Snyder \' Justice League', '242 min', 'Non m\'importa quanti demoni in quanti inferni. Non ha mai combattuto con noi. Non con noi uniti');
 
+var_dump($batman_v_superman, $justice_league);
