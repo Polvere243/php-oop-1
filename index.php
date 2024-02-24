f7468b5bd26ace42f002abb5ba7344f4f5a7aca6
Polvere243 <danielercoli@gmail.com>
2024-02-24 11:14:56 +0100

    added some comments and created a constructor

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
-
+// classe
 class Movies {
+    // variabili d'istanza
     public $title;
     public $plot;
     public $duration;
     public $earning;
+    
+    // costruttore
+    public function __construct($title, $plot, $duration, $earning) {
+        $this ->title = $title;
+        $this ->plot = $plot;
+        $this ->duration = $duration;
+        $this ->earning = $earning;
+    }
 }
