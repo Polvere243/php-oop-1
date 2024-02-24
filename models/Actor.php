45fae0b4b71a77a611f3de7dfdd5f6aaa68c6654
Polvere243 <danielercoli@gmail.com>
2024-02-24 17:36:42 +0100

    declared class Actor

--- models/Actor.php
+++ models/Actor.php
@@ -1,2 +1,16 @@
 <?php
+// dichiaro la classe
+class Actor
+{
+    // variabili d'istanza
+    public $first_name;
+    public $last_name;
+    public $role;
+    // costruttore
+    public function __construct($first_name, $last_name)
+    {
+        $this ->first_name = $first_name;
+        $this ->last_name = $last_name; 
+    }
 
+}
